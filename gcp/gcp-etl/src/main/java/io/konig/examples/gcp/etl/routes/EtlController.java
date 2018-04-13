7505c667b97cdfd8cd697a086e982795636bf2ad
zednis <lokipatera@gmail.com>
2018-04-13 09:28:08 -0600

    added comments to EtlController class

--- gcp/gcp-etl/src/main/java/io/konig/examples/gcp/etl/routes/EtlController.java
+++ gcp/gcp-etl/src/main/java/io/konig/examples/gcp/etl/routes/EtlController.java
@@ -15,13 +15,33 @@ public class EtlController extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        // listen to events on pubsub subscription {pubsub.projectId}:{pubsub.subsubscription}
         from("pubsub:{{pubsub.projectId}}:{{pubsub.subscription}}?"
                 + "maxMessagesPerPoll={{consumer.maxMessagesPerPoll}}&"
                 + "concurrentConsumers={{consumer.concurrentConsumers}}")
-                .routeId("fromGooglePubSub")
-                .filter(method("pubsubAttributeReader", "getEventType").isEqualTo("OBJECT_FINALIZE"))
+
+                // add a route identifier
+                .routeId("PersonETLRoute")
+
+                // add the eventType to the message header
+                .setHeader("eventType", method("pubsubAttributeReader", "getEventType"))
+
+                // filter events not describing the successful write of a file
+                .filter(simple("${header.eventType} == 'OBJECT_FINALIZE'"))
+
+                // convert message payload into a Java Map
                 .unmarshal().json(JsonLibrary.Jackson, Map.class)
-                .log("FILE ${body[name]}")
+
+                // add the filename added to the bucket to the message header
+                .setHeader("datafile", simple("${body[name]}"))
+
+                // TODO load extract datafile CSV into bigquery staging table
+                // TODO execute transforms
+
+                // log the extract datafile name
+                .log("${header.datafile}")
+
+                // write the message payload (the notification from the S3 bucket) to standard out
                 .to("stream:out");
 
     }
